455c366a5a2af9459131de8e1d4f98732d052169
iglesiaskevinralphbusiness <42959944+iglesiaskevinralphbusiness@users.noreply.github.com>
2018-10-03 23:03:35 +0800

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Movies from "./component/movies";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Movies />
+      </div>
+    );
+  }
+}
+
+export default App;
